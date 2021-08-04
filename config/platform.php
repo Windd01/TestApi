cb9652885eaea69e6f39352e21c1034293da4bea
KHAPU <daikhanh9260@gmail.com>
2021-08-04 10:23:32 +0700

    update

---
+++ config/platform.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'facebook' => [
+        'host' => 'graph.facebook.com',
+        'version' => 'v11.0',
+        'slugs' => [
+            'account' => '',
+            'long_time_token' => 'oauth/access_token',
+            'insights' => '{id}/insights',
+            'ads' => '{id}/ads'
+        ],
+    ]
+];
